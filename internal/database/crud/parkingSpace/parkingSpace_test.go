ffeb8e1bc3ffa7248ef07e122dfbee9c46e0ea71
Luca Morgado <luca.morgado.pro@gmail.com>
2025-02-14 16:33:15 +0100

    update crud unit test and add database unit tests

--- internal/database/crud/parkingSpace/parkingSpace_test.go
+++ internal/database/crud/parkingSpace/parkingSpace_test.go
@@ -2,242 +2,122 @@ package parkingSpace
 
 import (
 	"context"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 	"github.com/tot0p/CoursUT/internal/database"
 	"github.com/tot0p/CoursUT/internal/models"
-	"testing"
 )
 
-func TestCreateParkingSpace(t *testing.T) {
+func setupDatabase(t *testing.T) func() {
 	err := database.InitDatabase()
-	defer func() {
+	require.NoError(t, err)
+	return func() {
 		err := database.CloseDatabase()
-		if err != nil {
-			t.Error(err)
-		}
-	}()
-	if err != nil {
-		t.Error(err)
+		require.NoError(t, err)
 	}
-	var ParkingSpace = models.ParkingSpace{
-		SpaceNumber: "A32",
-	}
-	_, err = CreateParkingSpace(&ParkingSpace)
-	if err != nil {
-		t.Error(err)
-	}
-	// check if the parking space was created
-	if ParkingSpace.SpaceNumber == "" {
-		t.Error("Parking space was not created")
-	}
-	// query the parking space from the database
-	p := models.ParkingSpace{}
-	err = database.Conn.QueryRowContext(context.Background(), "SELECT * FROM parking_space WHERE id = ?;", ParkingSpace.ID).Scan(&p.ID, &p.SpaceNumber)
-	if err != nil {
-		t.Error(err)
-	}
-	// check if the parking space is the same
-	if p != ParkingSpace {
-		t.Error("Parking space is not the same")
-	}
+}
+
+func TestCreateParkingSpace(t *testing.T) {
+	defer setupDatabase(t)()
+
+	parkingSpace := models.ParkingSpace{SpaceNumber: "A32"}
+	_, err := CreateParkingSpace(&parkingSpace)
+	require.NoError(t, err)
+	assert.NotEmpty(t, parkingSpace.SpaceNumber)
+
+	var p models.ParkingSpace
+	err = database.Conn.QueryRowContext(context.Background(), "SELECT * FROM parking_space WHERE id = ?;", parkingSpace.ID).Scan(&p.ID, &p.SpaceNumber)
+	require.NoError(t, err)
+	assert.Equal(t, parkingSpace, p)
 }
 
 func TestCreateParkingSpaceAlreadyExists(t *testing.T) {
-	err := database.InitDatabase()
-	defer func() {
-		err := database.CloseDatabase()
-		if err != nil {
-			t.Error(err)
-		}
-	}()
-	if err != nil {
-		t.Error(err)
-	}
-	var ParkingSpace = models.ParkingSpace{
-		SpaceNumber: "A32",
-	}
-	_, err = CreateParkingSpace(&ParkingSpace)
-	if err != nil {
-		t.Error(err)
-	}
-	_, err = CreateParkingSpace(&ParkingSpace)
-	if err == nil {
-		t.Error("Parking space was created twice")
-	}
+	defer setupDatabase(t)()
+
+	parkingSpace := models.ParkingSpace{SpaceNumber: "A32"}
+	_, err := CreateParkingSpace(&parkingSpace)
+	require.NoError(t, err)
+
+	_, err = CreateParkingSpace(&parkingSpace)
+	assert.Error(t, err)
 }
 
 func TestGetParkingSpace(t *testing.T) {
-	err := database.InitDatabase()
-	defer func() {
-		err := database.CloseDatabase()
-		if err != nil {
-			t.Error(err)
-		}
-	}()
-	if err != nil {
-		t.Error(err)
-	}
-	var ParkingSpace = models.ParkingSpace{
-		SpaceNumber: "A32",
-	}
-	temp, err := CreateParkingSpace(&ParkingSpace)
-	if err != nil {
-		t.Error(err)
-	}
-	ParkingSpace = *temp
-	p, err := GetParkingSpace(ParkingSpace.ID)
-	if err != nil {
-		t.Error(err)
-	}
-	if p.SpaceNumber != ParkingSpace.SpaceNumber {
-		t.Error("Parking space is not the same")
-	}
-	if p.ID != ParkingSpace.ID {
-		t.Error("Parking space is not the same")
-	}
+	defer setupDatabase(t)()
+
+	parkingSpace := models.ParkingSpace{SpaceNumber: "A32"}
+	temp, err := CreateParkingSpace(&parkingSpace)
+	require.NoError(t, err)
+	parkingSpace = *temp
+
+	p, err := GetParkingSpace(parkingSpace.ID)
+	require.NoError(t, err)
+	assert.Equal(t, parkingSpace, *p)
 }
 
 func TestGetParkingSpaceNotFound(t *testing.T) {
-	err := database.InitDatabase()
-	defer func() {
-		err := database.CloseDatabase()
-		if err != nil {
-			t.Error(err)
-		}
-	}()
-	if err != nil {
-		t.Error(err)
-	}
-	_, err = GetParkingSpace(1)
-	if err == nil {
-		t.Error("Parking space was found")
-	}
+	defer setupDatabase(t)()
+
+	_, err := GetParkingSpace(1)
+	assert.Error(t, err)
 }
 
 func TestGetParkingSpaces(t *testing.T) {
-	err := database.InitDatabase()
-	defer func() {
-		err := database.CloseDatabase()
-		if err != nil {
-			t.Error(err)
-		}
-	}()
-	if err != nil {
-		t.Error(err)
-	}
-	var ParkingSpace = models.ParkingSpace{
-		SpaceNumber: "A32",
-	}
-	_, err = CreateParkingSpace(&ParkingSpace)
-	if err != nil {
-		t.Error(err)
-	}
+	defer setupDatabase(t)()
+
+	parkingSpace := models.ParkingSpace{SpaceNumber: "A32"}
+	_, err := CreateParkingSpace(&parkingSpace)
+	require.NoError(t, err)
+
 	parkingSpaces, err := GetParkingSpaces()
-	if err != nil {
-		t.Error(err)
-	}
-	if len(parkingSpaces) == 0 {
-		t.Error("No parking spaces found")
-	}
+	require.NoError(t, err)
+	assert.NotEmpty(t, parkingSpaces)
 }
 
 func TestDeleteParkingSpace(t *testing.T) {
-	err := database.InitDatabase()
-	defer func() {
-		err := database.CloseDatabase()
-		if err != nil {
-			t.Error(err)
-		}
-	}()
-	if err != nil {
-		t.Error(err)
-	}
-	var ParkingSpace = models.ParkingSpace{
-		SpaceNumber: "A32",
-	}
-	_, err = CreateParkingSpace(&ParkingSpace)
-	if err != nil {
-		t.Error(err)
-	}
-	err = DeleteParkingSpace(ParkingSpace.ID)
-	if err != nil {
-		t.Error(err)
-	}
-	_, err = GetParkingSpace(ParkingSpace.ID)
-	if err == nil {
-		t.Error("Parking space was not deleted")
-	}
+	defer setupDatabase(t)()
+
+	parkingSpace := models.ParkingSpace{SpaceNumber: "A32"}
+	_, err := CreateParkingSpace(&parkingSpace)
+	require.NoError(t, err)
+
+	err = DeleteParkingSpace(parkingSpace.ID)
+	require.NoError(t, err)
+
+	_, err = GetParkingSpace(parkingSpace.ID)
+	assert.Error(t, err)
 }
 
 func TestDeleteParkingSpaceNotFound(t *testing.T) {
-	err := database.InitDatabase()
-	defer func() {
-		err := database.CloseDatabase()
-		if err != nil {
-			t.Error(err)
-		}
-	}()
-	if err != nil {
-		t.Error(err)
-	}
-	err = DeleteParkingSpace(1)
-	if err == nil {
-		t.Error("no error returned")
-	}
+	defer setupDatabase(t)()
+
+	err := DeleteParkingSpace(1)
+	assert.Error(t, err)
 }
 
 func TestUpdateParkingSpace(t *testing.T) {
-	err := database.InitDatabase()
-	defer func() {
-		err := database.CloseDatabase()
-		if err != nil {
-			t.Error(err)
-		}
-	}()
-	if err != nil {
-		t.Error(err)
-	}
-	var ParkingSpace = models.ParkingSpace{
-		SpaceNumber: "A32",
-	}
-	temp, err := CreateParkingSpace(&ParkingSpace)
-	if err != nil {
-		t.Error(err)
-	}
-	ParkingSpace.ID = temp.ID
-	ParkingSpace.SpaceNumber = "A33"
-	err = UpdateParkingSpace(&ParkingSpace)
-	if err != nil {
-		t.Error(err)
-	}
-	p, err := GetParkingSpace(ParkingSpace.ID)
-	if err != nil {
-		t.Error(err)
-	}
-	if p.ID != ParkingSpace.ID {
-		t.Error("Parking space was not updated")
-	}
-	if p.SpaceNumber != "A33" {
-		t.Error("Parking space was not updated")
-	}
+	defer setupDatabase(t)()
+
+	parkingSpace := models.ParkingSpace{SpaceNumber: "A32"}
+	temp, err := CreateParkingSpace(&parkingSpace)
+	require.NoError(t, err)
+	parkingSpace.ID = temp.ID
+	parkingSpace.SpaceNumber = "A33"
+
+	err = UpdateParkingSpace(&parkingSpace)
+	require.NoError(t, err)
+
+	p, err := GetParkingSpace(parkingSpace.ID)
+	require.NoError(t, err)
+	assert.Equal(t, "A33", p.SpaceNumber)
 }
 
 func TestUpdateParkingSpaceNotFound(t *testing.T) {
-	err := database.InitDatabase()
-	defer func() {
-		err := database.CloseDatabase()
-		if err != nil {
-			t.Error(err)
-		}
-	}()
-	if err != nil {
-		t.Error(err)
-	}
-	var ParkingSpace = models.ParkingSpace{
-		ID:          1,
-		SpaceNumber: "A32",
-	}
-	err = UpdateParkingSpace(&ParkingSpace)
-	if err == nil {
-		t.Error("no error returned")
-	}
+	defer setupDatabase(t)()
+
+	parkingSpace := models.ParkingSpace{ID: 1, SpaceNumber: "A32"}
+	err := UpdateParkingSpace(&parkingSpace)
+	assert.Error(t, err)
 }
--- internal/database/crud/parkingSpaceInformation/parkingSpaceInformation_test.go
+++ internal/database/crud/parkingSpaceInformation/parkingSpaceInformation_test.go
@@ -2,310 +2,186 @@ package parkingSpaceInformation
 
 import (
 	"context"
-	"fmt"
-	"github.com/tot0p/CoursUT/internal/database"
 	"github.com/tot0p/CoursUT/internal/database/crud/parkingSpace"
 	"github.com/tot0p/CoursUT/internal/database/crud/vehicle"
-	"github.com/tot0p/CoursUT/internal/models"
 	"testing"
 	"time"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"github.com/tot0p/CoursUT/internal/database"
+	"github.com/tot0p/CoursUT/internal/models"
 )
 
-func TestCreateParkingSpaceInformation(t *testing.T) {
+func setupDatabase(t *testing.T) func() {
 	err := database.InitDatabase()
-	defer func() {
+	require.NoError(t, err)
+	return func() {
 		err := database.CloseDatabase()
-		if err != nil {
-			t.Error(err)
-		}
-	}()
-	if err != nil {
-		t.Error(err)
+		require.NoError(t, err)
 	}
-	var ParkingSpace = models.ParkingSpace{
-		SpaceNumber: "A32",
-	}
-	retParkingSpace, err := parkingSpace.CreateParkingSpace(&ParkingSpace)
-	if err != nil {
-		t.Error(err)
-	}
-	var Vehicle = models.Vehicle{
-		Plate:       "AA-123-AA",
-		VehicleType: models.Car,
-	}
-	retVehicle, err := vehicle.CreateVehicle(&Vehicle)
-	if err != nil {
-		t.Error(err)
-	}
-	var ParkingSpaceInformation = models.ParkingSpaceInformation{
+}
+
+func TestCreateParkingSpaceInformation(t *testing.T) {
+	defer setupDatabase(t)()
+
+	p1 := models.ParkingSpace{SpaceNumber: "A32"}
+	retParkingSpace, err := parkingSpace.CreateParkingSpace(&p1)
+	require.NoError(t, err)
+
+	v := models.Vehicle{Plate: "AA-123-AA", VehicleType: models.Car}
+	retVehicle, err := vehicle.CreateVehicle(&v)
+	require.NoError(t, err)
+
+	parkingSpaceInformation := models.ParkingSpaceInformation{
 		ParkingSpaceID:  retParkingSpace.ID,
 		VehicleID:       retVehicle.ID,
 		ArrivalTime:     time.Now(),
 		DepartureTime:   time.Now().Add(time.Hour),
 		ParkingDuration: time.Hour,
 	}
-	retParkingSpaceInformation, err := CreateParkingSpaceInformation(&ParkingSpaceInformation)
-	if err != nil {
-		t.Error(err)
-	}
-	// check if the parking space information was created
-	if retParkingSpaceInformation.ID == 0 {
-		t.Error("Parking space information was not created")
-	}
-	// query the parking space information from the database
-	p := models.ParkingSpaceInformation{}
+	retParkingSpaceInformation, err := CreateParkingSpaceInformation(&parkingSpaceInformation)
+	require.NoError(t, err)
+	assert.NotZero(t, retParkingSpaceInformation.ID)
+
+	var p models.ParkingSpaceInformation
 	err = database.Conn.QueryRowContext(context.Background(), "SELECT * FROM parking_space_information WHERE id = ?;", retParkingSpaceInformation.ID).Scan(&p.ID, &p.ParkingSpaceID, &p.VehicleID, &p.ArrivalTime, &p.DepartureTime, &p.ParkingDuration)
-	if err != nil {
-		t.Error(err)
-	}
-	// check if the parking space information is the same
-	if p.ID != retParkingSpaceInformation.ID {
-		t.Error("Parking space information is not the same")
-	}
+	require.NoError(t, err)
+	assert.Equal(t, retParkingSpaceInformation.ID, p.ID)
 }
 
 func TestGetParkingSpaceInformation(t *testing.T) {
-	err := database.InitDatabase()
-	defer func() {
-		err := database.CloseDatabase()
-		if err != nil {
-			t.Error(err)
-		}
-	}()
-	if err != nil {
-		t.Error(err)
-	}
-	var ParkingSpace = models.ParkingSpace{
-		SpaceNumber: "A32",
-	}
-	retParkingSpace, err := parkingSpace.CreateParkingSpace(&ParkingSpace)
-	if err != nil {
-		t.Error(err)
-	}
-	var Vehicle = models.Vehicle{
-		Plate:       "AA-123-AA",
-		VehicleType: models.Car,
-	}
-	retVehicle, err := vehicle.CreateVehicle(&Vehicle)
-	if err != nil {
-		t.Error(err)
-	}
-	var ParkingSpaceInformation = models.ParkingSpaceInformation{
+	defer setupDatabase(t)()
+
+	p := models.ParkingSpace{SpaceNumber: "A32"}
+	retParkingSpace, err := parkingSpace.CreateParkingSpace(&p)
+	require.NoError(t, err)
+
+	v := models.Vehicle{Plate: "AA-123-AA", VehicleType: models.Car}
+	retVehicle, err := vehicle.CreateVehicle(&v)
+	require.NoError(t, err)
+
+	parkingSpaceInformation := models.ParkingSpaceInformation{
 		ParkingSpaceID:  retParkingSpace.ID,
 		VehicleID:       retVehicle.ID,
 		ArrivalTime:     time.Now(),
 		DepartureTime:   time.Now().Add(time.Hour),
 		ParkingDuration: time.Hour,
 	}
-	retParkingSpaceInformation, err := CreateParkingSpaceInformation(&ParkingSpaceInformation)
-	if err != nil {
-		t.Error(err)
-	}
+	retParkingSpaceInformation, err := CreateParkingSpaceInformation(&parkingSpaceInformation)
+	require.NoError(t, err)
+
 	retParkingSpaceInformation2, err := GetParkingSpaceInformation(retParkingSpaceInformation.ID)
-	if err != nil {
-		t.Error(err)
-	}
-	if retParkingSpaceInformation.ID != retParkingSpaceInformation2.ID {
-		t.Error("Parking space information is not the same")
-	}
+	require.NoError(t, err)
+	assert.Equal(t, retParkingSpaceInformation.ID, retParkingSpaceInformation2.ID)
 }
 
 func TestGetParkingSpaceInformationNotFound(t *testing.T) {
-	err := database.InitDatabase()
-	defer func() {
-		err := database.CloseDatabase()
-		if err != nil {
-			t.Error(err)
-		}
-	}()
-	if err != nil {
-		t.Error(err)
-	}
-	_, err = GetParkingSpaceInformation(0)
-	if err == nil {
-		t.Error("Parking space information was found")
-	}
+	defer setupDatabase(t)()
+
+	_, err := GetParkingSpaceInformation(0)
+	assert.Error(t, err)
 }
 
 func TestGetParkingSpaceInformations(t *testing.T) {
-	err := database.InitDatabase()
-	defer func() {
-		err := database.CloseDatabase()
-		if err != nil {
-			t.Error(err)
-		}
-	}()
-	if err != nil {
-		t.Error(err)
-	}
-	var ParkingSpace = models.ParkingSpace{
-		SpaceNumber: "A32",
-	}
-	retParkingSpace, err := parkingSpace.CreateParkingSpace(&ParkingSpace)
-	if err != nil {
-		t.Error(err)
-	}
-	var Vehicle = models.Vehicle{
-		Plate:       "AA-123-AA",
-		VehicleType: models.Car,
-	}
-	retVehicle, err := vehicle.CreateVehicle(&Vehicle)
-	if err != nil {
-		t.Error(err)
-	}
-	var ParkingSpaceInformation = models.ParkingSpaceInformation{
+	defer setupDatabase(t)()
+
+	p := models.ParkingSpace{SpaceNumber: "A32"}
+	retParkingSpace, err := parkingSpace.CreateParkingSpace(&p)
+	require.NoError(t, err)
+
+	v := models.Vehicle{Plate: "AA-123-AA", VehicleType: models.Car}
+	retVehicle, err := vehicle.CreateVehicle(&v)
+	require.NoError(t, err)
+
+	parkingSpaceInformation := models.ParkingSpaceInformation{
 		ParkingSpaceID:  retParkingSpace.ID,
 		VehicleID:       retVehicle.ID,
 		ArrivalTime:     time.Now(),
 		DepartureTime:   time.Now().Add(time.Hour),
 		ParkingDuration: time.Hour,
 	}
-	_, err = CreateParkingSpaceInformation(&ParkingSpaceInformation)
-	if err != nil {
-		t.Error(err)
-	}
+	_, err = CreateParkingSpaceInformation(&parkingSpaceInformation)
+	require.NoError(t, err)
+
 	retParkingSpaceInformations, err := GetParkingSpaceInformations()
-	if err != nil {
-		t.Error(err)
-	}
-	if len(retParkingSpaceInformations) == 0 {
-		t.Error("No parking space information found")
-	}
+	require.NoError(t, err)
+	assert.NotEmpty(t, retParkingSpaceInformations)
 }
 
 func TestDeleteParkingSpaceInformation(t *testing.T) {
-	err := database.InitDatabase()
-	defer func() {
-		err := database.CloseDatabase()
-		if err != nil {
-			t.Error(err)
-		}
-	}()
-	if err != nil {
-		t.Error(err)
-	}
-	var ParkingSpace = models.ParkingSpace{
-		SpaceNumber: "A32",
-	}
-	retParkingSpace, err := parkingSpace.CreateParkingSpace(&ParkingSpace)
-	if err != nil {
-		t.Error(err)
-	}
-	var Vehicle = models.Vehicle{
-		Plate:       "AA-123-AA",
-		VehicleType: models.Car,
-	}
-	retVehicle, err := vehicle.CreateVehicle(&Vehicle)
-	if err != nil {
-		t.Error(err)
-	}
-	var ParkingSpaceInformation = models.ParkingSpaceInformation{
+	defer setupDatabase(t)()
+
+	p := models.ParkingSpace{SpaceNumber: "A32"}
+	retParkingSpace, err := parkingSpace.CreateParkingSpace(&p)
+	require.NoError(t, err)
+
+	v := models.Vehicle{Plate: "AA-123-AA", VehicleType: models.Car}
+	retVehicle, err := vehicle.CreateVehicle(&v)
+	require.NoError(t, err)
+
+	parkingSpaceInformation := models.ParkingSpaceInformation{
 		ParkingSpaceID:  retParkingSpace.ID,
 		VehicleID:       retVehicle.ID,
 		ArrivalTime:     time.Now(),
 		DepartureTime:   time.Now().Add(time.Hour),
 		ParkingDuration: time.Hour,
 	}
-	retParkingSpaceInformation, err := CreateParkingSpaceInformation(&ParkingSpaceInformation)
-	if err != nil {
-		t.Error(err)
-	}
+	retParkingSpaceInformation, err := CreateParkingSpaceInformation(&parkingSpaceInformation)
+	require.NoError(t, err)
+
 	err = DeleteParkingSpaceInformation(retParkingSpaceInformation.ID)
-	if err != nil {
-		t.Error(err)
-	}
+	require.NoError(t, err)
+
 	_, err = GetParkingSpaceInformation(retParkingSpaceInformation.ID)
-	if err == nil {
-		t.Error("Parking space information was found")
-	}
+	assert.Error(t, err)
 }
 
 func TestDeleteParkingSpaceInformationNotFound(t *testing.T) {
+	defer setupDatabase(t)()
+
 	err := DeleteParkingSpaceInformation(0)
-	if err == nil {
-		t.Error("Parking space information was found")
-	}
+	assert.Error(t, err)
 }
 
 func TestUpdateParkingSpaceInformation(t *testing.T) {
-	err := database.InitDatabase()
-	defer func() {
-		err := database.CloseDatabase()
-		if err != nil {
-			t.Error(err)
-		}
-	}()
-	if err != nil {
-		t.Error(err)
-	}
-	var ParkingSpace = models.ParkingSpace{
-		SpaceNumber: "A32",
-	}
-	retParkingSpace, err := parkingSpace.CreateParkingSpace(&ParkingSpace)
-	if err != nil {
-		t.Error(err)
-	}
-	var Vehicle = models.Vehicle{
-		Plate:       "AA-123-AA",
-		VehicleType: models.Car,
-	}
-	retVehicle, err := vehicle.CreateVehicle(&Vehicle)
-	if err != nil {
-		t.Error(err)
-	}
-	var ParkingSpaceInformation = models.ParkingSpaceInformation{
+	defer setupDatabase(t)()
+
+	p := models.ParkingSpace{SpaceNumber: "A32"}
+	retParkingSpace, err := parkingSpace.CreateParkingSpace(&p)
+	require.NoError(t, err)
+
+	v := models.Vehicle{Plate: "AA-123-AA", VehicleType: models.Car}
+	retVehicle, err := vehicle.CreateVehicle(&v)
+	require.NoError(t, err)
+
+	parkingSpaceInformation := models.ParkingSpaceInformation{
 		ParkingSpaceID:  retParkingSpace.ID,
 		VehicleID:       retVehicle.ID,
 		ArrivalTime:     time.Now(),
 		DepartureTime:   time.Now().Add(time.Hour),
 		ParkingDuration: time.Hour,
 	}
-	retParkingSpaceInformation, err := CreateParkingSpaceInformation(&ParkingSpaceInformation)
-	if err != nil {
-		t.Error(err)
-	}
+	retParkingSpaceInformation, err := CreateParkingSpaceInformation(&parkingSpaceInformation)
+	require.NoError(t, err)
+
 	retParkingSpaceInformation.ArrivalTime = time.Now().Add(time.Hour)
 	retParkingSpaceInformation.DepartureTime = time.Now().Add(2 * time.Hour)
 	retParkingSpaceInformation.ParkingDuration = 2 * time.Hour
 	err = UpdateParkingSpaceInformation(retParkingSpaceInformation)
-	if err != nil {
-		t.Error(err)
-	}
+	require.NoError(t, err)
+
 	retParkingSpaceInformation2, err := GetParkingSpaceInformation(retParkingSpaceInformation.ID)
-	if err != nil {
-		t.Error(err)
-	}
-	fmt.Println(retParkingSpaceInformation.ArrivalTime.Unix())
-	fmt.Println(retParkingSpaceInformation2.ArrivalTime.Unix())
-	fmt.Println(retParkingSpaceInformation.ID)
-	fmt.Println(retParkingSpaceInformation2.ID)
-	if retParkingSpaceInformation2.ID != retParkingSpaceInformation.ID {
-		t.Error("Parking space information is not the same")
-	}
-	if retParkingSpaceInformation2.ArrivalTime.Unix() != retParkingSpaceInformation.ArrivalTime.Unix() {
-		t.Error("Parking space information is not the same")
-	}
-	if retParkingSpaceInformation2.DepartureTime.Unix() != retParkingSpaceInformation.DepartureTime.Unix() {
-		t.Error("Parking space information is not the same")
-	}
-	if retParkingSpaceInformation2.ParkingDuration != retParkingSpaceInformation.ParkingDuration {
-		t.Error("Parking space information is not the same")
-	}
+	require.NoError(t, err)
+	assert.Equal(t, retParkingSpaceInformation.ID, retParkingSpaceInformation2.ID)
+	assert.Equal(t, retParkingSpaceInformation.ArrivalTime.Unix(), retParkingSpaceInformation2.ArrivalTime.Unix())
+	assert.Equal(t, retParkingSpaceInformation.DepartureTime.Unix(), retParkingSpaceInformation2.DepartureTime.Unix())
+	assert.Equal(t, retParkingSpaceInformation.ParkingDuration, retParkingSpaceInformation2.ParkingDuration)
 }
 
 func TestUpdateParkingSpaceInformationNotFound(t *testing.T) {
-	err := database.InitDatabase()
-	defer func() {
-		err := database.CloseDatabase()
-		if err != nil {
-			t.Error(err)
-		}
-	}()
-	if err != nil {
-		t.Error(err)
-	}
-	err = UpdateParkingSpaceInformation(&models.ParkingSpaceInformation{})
-	if err == nil {
-		t.Error("Parking space information was found")
-	}
+	defer setupDatabase(t)()
+
+	err := UpdateParkingSpaceInformation(&models.ParkingSpaceInformation{})
+	assert.Error(t, err)
 }
