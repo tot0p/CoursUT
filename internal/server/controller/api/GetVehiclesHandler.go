88d0462cd9519a16b196c1a6c93ad2fe4eac5ab1
tot0p <thomas.lemaitre.78420@gmail.com>
2025-02-14 10:52:11 +0100

    add GetVehiclesHandlers and ut

--- internal/server/controller/api/GetVehiclesHandler.go
+++ internal/server/controller/api/GetVehiclesHandler.go
@@ -8,9 +8,13 @@ import (
 func GetVehiclesHandler(c *fiber.Ctx) error {
 	vehicles, err := vehicle.GetVehicles()
 	if err != nil {
+
 		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
 			"error": err.Error(),
 		})
+	}
+	if vehicles == nil || len(vehicles) == 0 {
+		return c.Status(fiber.StatusNoContent).Send(nil)
 	}
 	return c.JSON(vehicles)
 }
