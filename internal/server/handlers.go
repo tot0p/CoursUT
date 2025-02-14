66ae32e7d24f5bb4133e59ebb803fd91437f6089
tot0p <thomas.lemaitre.78420@gmail.com>
2025-02-14 12:38:02 +0100

    move vehicle controller

--- internal/server/handlers.go
+++ internal/server/handlers.go
@@ -1,16 +1,16 @@
 package server
 
 import (
-	apiHandler "github.com/tot0p/CoursUT/internal/server/controller/api"
+	"github.com/tot0p/CoursUT/internal/server/controller/api/VehicleController"
 )
 
 func (serv *Server) handlers() {
 
 	// api group
 	api := serv.fiberApp.Group("/api")
-	api.Get("/ping", apiHandler.PingHandler)
-	api.Post("/vehicles", apiHandler.AddVehicleHandler)
-	api.Get("/vehicles", apiHandler.GetVehiclesHandler)
-	api.Delete("/vehicles/:id", apiHandler.DeleteVehicleHandler)
-	api.Put("/vehicles/:id", apiHandler.UpdateVehicleHandler)
+	api.Get("/ping", VehicleController.PingHandler)
+	api.Post("/vehicles", VehicleController.AddVehicleHandler)
+	api.Get("/vehicles", VehicleController.GetVehiclesHandler)
+	api.Delete("/vehicles/:id", VehicleController.DeleteVehicleHandler)
+	api.Put("/vehicles/:id", VehicleController.UpdateVehicleHandler)
 }
